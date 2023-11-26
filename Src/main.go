fe4404438e84e38e9e747d91b22bf798622ad52b
Daniel Guzman <danielsonguzman2000@gmail.com>
2023-11-26 12:52:34 -0800

    Created Bubbletea model

--- Src/main.go
+++ Src/main.go
@@ -13,13 +13,6 @@ type model struct {
 	selected map[int]struct{} // which items are selected
 }
 
-func initialModel() model {
-	return model{
-		choices:  []string{"Buy Pizza", "Buy Bread sticks", "Buy Corn dogs"},
-		selected: make(map[int]struct{}),
-	}
-}
-
 func (m model) Init() tea.Cmd {
 	return nil
 }
@@ -28,13 +21,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "crtl+c", "q":
+		case "crtl+c", "q": // Quit the application
 			return m, tea.Quit
-		case "up", "k":
+		case "up", "k": // Move cursor up
 			if m.cursor > 0 {
 				m.cursor--
 			}
-		case "down", "j":
+		case "down", "j": // Move cursor down
 			if m.cursor < len(m.choices)-1 {
 				m.cursor++
 			}
@@ -54,6 +47,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	s := "What would you like to buy at the market?\n\n"
 
+	// Iterating through the choices
 	for i, choice := range m.choices {
 
 		// Is the cursor pointing at this choice?
@@ -74,6 +68,13 @@ func (m model) View() string {
 
 	s += "\nPress q to quit.\n"
 	return s
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Buy Pizza", "Buy Bread sticks", "Buy Corn dogs"},
+		selected: make(map[int]struct{}),
+	}
 }
 
 func main() {
